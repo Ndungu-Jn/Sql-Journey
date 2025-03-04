10f24a9951d4e853db6acda2c6c2f451b5fe5e9d
Ndungu-Jn <ndungulink@gmail.com>
2025-03-04 06:04:20 +0300

    used aliasing to rename the table

--- joins.sql
+++ joins.sql
@@ -9,6 +9,6 @@ SELECT *
 FROM employee_salary;
 
 SELECT *
-FROM employee_demographics
-INNER JOIN employee_salary  -- returns rows that are the same in lboth columns from both tables.
-	ON employee_demographics.employee_id = employee_salary.employee_id;   -- the key  word ON is used to specify to which table to combine. 
+FROM employee_demographics AS demographics -- used aliasing to make the names shorter.
+INNER JOIN employee_salary  AS salary -- returns rows that are the same in lboth columns from both tables.
+	ON demographics.employee_id = salary.employee_id;   -- the key  word ON is used to specify to which table to combine. 
