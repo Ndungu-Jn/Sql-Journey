e8acde635cc855e20fa29be1d7e1b0841b9422e7
Ndungu-Jn <ndungulink@gmail.com>
2025-03-05 06:22:03 +0300

    inner join

--- joins.sql
+++ joins.sql
@@ -1,6 +1,6 @@
--- INNER JOIN 
-
 -- Joins -- Allow one to combine two tables or more together if  they have a common column
+
+-- INNER JOIN --used to reterieve records that have matching values in both tables being joined. if there is no match, the rows are excludes from the results
 
 SELECT *
 FROM employee_demographics;
@@ -9,6 +9,9 @@ SELECT *
 FROM employee_salary;
 
 SELECT *
-FROM employee_demographics AS demographics -- used aliasing to make the names shorter.
-INNER JOIN employee_salary  AS salary -- returns rows that are the same in lboth columns from both tables.
+FROM employee_demographics AS demographics              -- used aliasing to make the names shorter.
+INNER JOIN employee_salary  AS salary                   -- returns rows that are the same in lboth columns from both tables.
 	ON demographics.employee_id = salary.employee_id;   -- the key  word ON is used to specify to which table to combine. 
+
+
+-- OUTER JOIN --    
