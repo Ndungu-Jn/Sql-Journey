24ef4fb8c99186f9cc844d25e2bfd8f306a0b013
Ndungu-Jn <ndungulink@gmail.com>
2025-03-06 06:34:42 +0300

    case scenario for age layoffs in a company

--- Unions.sql
+++ Unions.sql
@@ -8,3 +8,12 @@
  UNION ALL                       --  combines all the rows, even the duplicates.
  SELECT first_name, last_name
  FROM employee_salary;
+
+ -- case scenario  is if a company wants to layoff older people and as well as some of the people that are paid handsomely
+ SELECT first_name, last_name, "Old Man" AS label
+ FROM employee_demographics
+ WHERE age > 40 AND gender = 'Male'
+ UNION
+ SELECT first_name, last_name, "Old Lady" AS label
+ FROM employee_demographics
+ WHERE age > 40 AND gender = 'Female'
