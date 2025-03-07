5cb8dce547355a2d3923101cd2c913c068ce57f4
Ndungu-Jn <ndungulink@gmail.com>
2025-03-07 07:10:15 +0300

    substring, getting characters form a specified index

--- functions.sql
+++ functions.sql
@@ -15,5 +15,10 @@ FROM employee_demographics;
 -- There is rtrim and ltrim
 SELECT trim('           SKY          ');
 
-select first_name, LEFT(first_name, 4)  --  How many characters from the left hand side do we want to select
+select first_name, 
+LEFT(first_name, 4)  --  How many characters from the left hand side do we want to select
+RIGHT(first_name, 4),  -- characters from the right side
+SUBSTRING(first_name, 3,2), -- takes the  characters at the specific position. i.e we wantt to get the month everyone was born in.
+birth_date,
+SUBSTRING(birth_date, 6,2) AS birth_month
 FROM employee_demographics;
