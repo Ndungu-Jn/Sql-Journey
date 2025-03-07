6176c2e35721f244cacffbec51e6ca46fb1c234f
Ndungu-Jn <ndungulink@gmail.com>
2025-03-07 06:59:54 +0300

    trimming

--- functions.sql
+++ functions.sql
@@ -10,3 +10,10 @@ SELECT LOWER('SKY');
 
 SELECT first_name, UPPER(first_name)  -- this gets the  names of the employees TO UPPER CASE.
 FROM employee_demographics;
+
+-- TRIM -- takes the white space at the end or at the front and gets rid of it
+-- There is rtrim and ltrim
+SELECT trim('           SKY          ');
+
+select first_name, LEFT(first_name, 4)  --  How many characters from the left hand side do we want to select
+FROM employee_demographics;
