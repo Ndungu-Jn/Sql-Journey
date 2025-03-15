03f91dc94cab65e638c23adad195155a407c1231
Ndungu-Jn <ndungulink@gmail.com>
2025-03-15 08:08:06 +0300

    Used the case statement to add logic. pay increase and bonuses

--- case_statements.sql
+++ case_statements.sql
@@ -9,3 +9,24 @@ CASE
     WHEN age >= 50 THEN "going is going"
 END AS Age_Bracket    
 FROM employee_demographics;
+
+-- Pay Increase and Bonus
+-- < 50000 = 5%
+-- > 50000 = 7%
+-- Finance = 10% bonus
+
+SELECT first_name, last_name, salary,
+CASE
+	WHEN salary < 50000 THEN salary * 1.05
+    WHEN salary > 50000 THEN salary * 1.07
+END AS Salary_Increase,
+CASE 
+	WHEN dept_id = 6 THEN salary * .10
+END AS Bonus    
+FROM employee_salary;
+
+-- Finance department gets a bonus.
+SELECT *
+FROM employee_salary;
+SELECT *
+FROM parks_departments
