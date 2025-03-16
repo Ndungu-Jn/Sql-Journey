7b2d42e0c41c040c33dabec55e03b9059d4adba9
Ndungu-Jn <ndungulink@gmail.com>
2025-03-16 08:19:20 +0300

    row_number, rank and dense_rank

--- Window_Functioon.sql
+++ Window_Functioon.sql
@@ -19,7 +19,9 @@ JOIN employee_salary AS sal
 	ON dem.employee_id = sal.employee_id  
 
 SELECT dem.employee_id, dem.first_name, dem.last_name, gender, salary,
-ROW_NUMBER() OVER(PARTITION BY gender ORDER  BY salary DESC)
+ROW_NUMBER() OVER(PARTITION BY gender ORDER  BY salary DESC) AS row_num,   -- gives the row a number -- no duplicates
+RANK() OVER(PARTITION BY gender ORDER  BY salary DESC) AS rank_num,       -- gives the rank  --allows duplicates if there is any based of of the duplicates. -- next number is usually the next number positionally
+DENSE_RANK() OVER(PARTITION BY gender ORDER  BY salary DESC) AS dense_rank_num   -- difference form rank is that its next number is going to be numerically
 FROM employee_demographics AS dem
 JOIN employee_salary AS sal
 	ON dem.employee_id = sal.employee_id     -- partitions and gets from highest to lowest.
