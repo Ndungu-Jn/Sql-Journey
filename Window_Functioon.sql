4ae168a66eb0bc0dec3da1055c3dd9a6d23483d3
Ndungu-Jn <ndungulink@gmail.com>
2025-03-16 08:03:28 +0300

    window functions

---
+++ Window_Functioon.sql
@@ -0,0 +1,19 @@
+-- windows Functions
+
+SELECT gender, AVG(salary) AS avg_salary
+FROM employee_demographics AS dem
+JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender; 
+
+SELECT dem.first_name, dem.last_name, gender, AVG(salary) OVER(PARTITION BY gender )  -- window function.
+FROM employee_demographics AS dem
+JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id;
+    
+    
+SELECT dem.first_name, dem.last_name, gender, salary,
+SUM(salary) OVER(PARTITION BY gender ORDER BY dem.employee_id )  AS Rolling_Total-- window function.
+FROM employee_demographics AS dem
+JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id  
