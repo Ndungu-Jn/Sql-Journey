382ce27d59cf1ad24c4a2ad7f8ba6dc53d3c974c
Ndungu-Jn <ndungulink@gmail.com>
2025-03-16 06:18:45 +0300

    All about subqueries

---
+++ subqueries.sql
@@ -0,0 +1,19 @@
+-- Subqueries --it a query inside another query.
+
+SELECT *
+FROM employee_demographics
+WHERE employee_id IN 
+				(SELECT employee_id, dept_id
+					FROM employee_salary
+                    WHERE dept_id = 1);
+
+
+select first_name, salary, 
+(SELECT AVG(salary)
+FROM employee_salary)
+FROM employee_salary;
+
+
+SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age)
+FROM employee_demographics
+GROUP BY gender;
