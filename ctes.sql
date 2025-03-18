5369741380ce3f2017a87f1e54e17efc3bea62b5
Ndungu-Jn <ndungulink@gmail.com>
2025-03-18 05:24:27 +0300

    Used the ctes to get the average

--- ctes.sql
+++ ctes.sql
@@ -1,10 +1,15 @@
 -- CTEs -- common table expressions -- just like subqueries but more standerdized
 -- the key word is WITH and can only be used after you create it
 
-
-SELECT gender, AVG(salary), MAX(salary), MIN(salary), COUNT(salary)
+WITH CTE_Example AS
+(
+SELECT gender, AVG(salary) avg_sal, MAX(salary) max_sal, MIN(salary) min_sal, COUNT(salary) count_sal
 FROM employee_demographics dem
 JOIN employee_salary sal
 	ON dem.employee_id = sal.employee_id
 GROUP BY gender
+)
+
+SELECT AVG(avg_sal)    -- makes the calculations super fast
+FROM CTE_example;
     
