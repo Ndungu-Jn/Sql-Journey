2716e9c0c88e3d2aca6c141770c7431af89229bf
Ndungu-Jn <ndungulink@gmail.com>
2025-03-19 06:01:04 +0300

    Introduced Delimiters  for best practices

--- Stored_procedures.sql
+++ Stored_procedures.sql
@@ -1,7 +1,7 @@
 --  Stored Procedures -- way to store the code so  its can be reusable -- its just like a function in other programming languages.
 -- the key word is CREATE PROCEDURE
 
-
+-- This is not best practice
 SELECT *
 FROM employee_salary
 WHERE salary >= 50000;
@@ -12,3 +12,19 @@ FROM employee_salary
 WHERE salary >= 50000;
 
 CALL large_salaries()
+
+-- NOW THIS IS BEST PRACTICE
+DELIMITER $$
+
+CREATE PROCEDURE large_salaries3()
+BEGIN
+	SELECT *
+	FROM employee_salary
+	WHERE salary >= 50000;
+	SELECT *
+	FROM employee_salary
+	WHERE salary >= 10000;
+END $$
+DELIMITER ;
+
+CALL large_salaries3()
