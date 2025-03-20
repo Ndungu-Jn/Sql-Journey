901e0ca2166d8d6f4a46a42679844f648082ed76
Ndungu-Jn <ndungulink@gmail.com>
2025-03-20 05:57:15 +0300

    A trigger to insert new values

---
+++ Triggers.sql
@@ -0,0 +1,23 @@
+-- Triggers  and events -- trigger is a block of code that excecutes automatically when an event takes place
+
+SELECT *
+FROM employee_demographics;
+
+SELECT *
+FROM employee_salary;
+
+DELIMITER $$
+CREATE  TRIGGER employee_insert
+	AFTER INSERT ON employee_salary
+    FOR EACH ROW 
+BEGIN
+	INSERT INTO employee_demographics( employee_id, first_name, last_name)
+    VALUES (NEW.employee_id, NEW.first_name, NEW.last_name);
+END $$
+DELIMITER ;
+
+INSERT INTO employee_salary (employee_id, first_name, last_name, occupation, salary, dept_id)
+VALUES(13, 'Mark', 'Kamau', 'Agentic CTO', '1000000', NULL);
+
+
+
